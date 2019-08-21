571bd9c5c9e6fc22ce7985dd50120cd1c339f317
Justin Bresee <justinbresee@gmail.com>
2019-08-21 09:35:06 -0600

    added some text to the about page

--- src/app.js
+++ src/app.js
@@ -106,5 +106,5 @@ app.get('*', (req, res) => {
 
 // SET UP SERVER TO LISTEN ON PORT 3000
 app.listen(port, () => {
-    console.log('Server is up on port' + port)
+    console.log('Server is up on port ' + port)
 })
